58462d115f9cf4b0ca8be9074c39e33fcc790bb3
artreysan <al2173038770@azc.uam.mx>
2022-09-08 17:13:39 -0500

    pagestatic Install MariaDB

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,21 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+   <React.StrictMode>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+          <a href="https://flowbite.com" className="flex items-center">
+              <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          </a>
+          <div className="flex items-center">
+              <a href="tel:5541251234" className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
+              <a href="#" className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+          </div>
+      </div>
+      </nav> 
+          
   </React.StrictMode>
 )
+
+
